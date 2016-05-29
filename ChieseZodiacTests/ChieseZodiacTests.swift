918f12d8f1cce3b3abb8fe7f2706198e32126504
wcgh <821272852@qq.com>
2016-05-29 17:07:21 +0800

    Initial Commit

---
+++ ChieseZodiacTests/ChieseZodiacTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChieseZodiacTests.swift
+//  ChieseZodiacTests
+//
+//  Created by wencai on 16/5/29.
+//  Copyright © 2016年 wencai. All rights reserved.
+//
+
+import XCTest
+@testable import ChieseZodiac
+
+class ChieseZodiacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
