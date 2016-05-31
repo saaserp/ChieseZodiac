2a7a9ac73285c89df448ac2a3c4215d005063884
wcgh <821272852@qq.com>
2016-05-31 10:49:34 +0800

    十二生肖小程序

--- ChieseZodiac/ViewController.swift
+++ ChieseZodiac/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var year: UITextField!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func okTapped(sender: AnyObject) {
+        if let myear = Int(year.text!) {
+            var imageNumber=(myear-4)%12
+            image.image=UIImage(named:String(imageNumber))
+        }else{
+        
+        }
+    
+    
+    }
 
 }
 
